1f8638d103b6b486ec7d689612624ce645a8d085
okadirkartal <161307071@kocaeli.edu.tr>
2019-12-13 22:52:50 +0300

    added sample service for city

---
+++ src/Services/Controllers/CityServiceController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataSource;
+using DataSource.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Services.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherServiceController : ControllerBase
+    {
+        private readonly ILogger<WeatherServiceController> _logger;
+        private readonly IDataRetriever<Weather> _dataRetriever;
+
+        public WeatherServiceController(ILogger<WeatherServiceController> logger, IDataRetriever<Weather> dataRetriever)
+        {
+            _logger = logger;
+            _dataRetriever = dataRetriever;
+        }
+    }
+}
