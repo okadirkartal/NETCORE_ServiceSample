63f4fcb05fd000c46ad25ebc18f49ebd0ae8830d
okadirkartal <161307071@kocaeli.edu.tr>
2019-12-14 20:34:32 +0300

    added timeZone method for GeoLocation api

--- src/Services/Controllers/GeoLocationServiceController.cs
+++ src/Services/Controllers/GeoLocationServiceController.cs
@@ -1,4 +1,5 @@
-﻿using System; 
+﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using DataSource;
 using DataSource.Entities;
@@ -14,20 +15,22 @@ namespace Services.Controllers
         private readonly ILogger<GeoLocationServiceController> _logger;
         private readonly IDataRetriever<GeoLocation> _dataRetriever;
 
-        public GeoLocationServiceController(ILogger<GeoLocationServiceController> logger, 
+        public GeoLocationServiceController(ILogger<GeoLocationServiceController> logger,
             IDataRetriever<GeoLocation> dataRetriever)
         {
             _logger = logger;
             _dataRetriever = dataRetriever;
         }
 
-        [HttpPost("{format?}"),FormatFilter]
-        public async Task<GeoLocation> Post([FromBody]string zipCode)
+      
+        [HttpPost]
+        [Route("all.{format}"),FormatFilter]
+        public async Task<GeoLocation> Post([FromBody] string zipCode)
         {
             try
             {
-               var result = await _dataRetriever.GetData(zipCode);
-               return result?[0];
+                var result = await _dataRetriever.GetData(zipCode);
+                return result?[0];
             }
             catch (Exception ex)
             {
@@ -35,20 +38,21 @@ namespace Services.Controllers
                 return null;
             }
         }
-//        
-//        [HttpPost]
-//        public async Task<string> GetTimeZone([FromBody]string zipCode)
-//        {
-//            try
-//            {
-//                var result = await _dataRetriever.GetData(zipCode);// _dataRetriever.GetData(cityCode);
-//                return result?[0]?.timezone;
-//            }
-//            catch (Exception ex)
-//            {
-//                _logger.LogError(ex.Message);
-//                return null;
-//            }
-//        }
+
+        [HttpPost]
+        [Route("timeZone.{format}"), FormatFilter]
+        public async Task<Timezone> GetTimeZone([FromBody] string zipCode)
+        {
+            try
+            {
+                var result = await _dataRetriever.GetData(zipCode);
+                return (result.Any()) ? result[0].timezone : null;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                return null;
+            }
+        }
     }
 }
