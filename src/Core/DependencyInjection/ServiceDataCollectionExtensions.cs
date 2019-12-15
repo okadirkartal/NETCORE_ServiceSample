1cf67c3ad16ca9c64c99febccbfa2dd9750b71f7
okadirkartal <161307071@kocaeli.edu.tr>
2019-12-15 18:43:05 +0300

    removed unnecessary binding for DataRetriever classes

--- src/Core/DependencyInjection/ServiceDataCollectionExtensions.cs
+++ src/Core/DependencyInjection/ServiceDataCollectionExtensions.cs
@@ -12,13 +12,10 @@ namespace DependencyInjection
         public static IServiceCollection AddServiceRetrieverServiceCollection(this IServiceCollection services)
         {
             services.AddScoped<IOfflineDataRetriever<City>, CityDataRetriever>();
+         
             services.TryAddScoped(typeof(IDataRetriever<WeatherInfo>), typeof(WeatherDataRetriever));
-            services.TryAddScoped(typeof(IOnlineDataRetriever), typeof(WeatherDataRetriever));
-            services.TryAddScoped(typeof(IOfflineDataRetriever<WeatherInfo>), typeof(WeatherDataRetriever));
 
             services.TryAddScoped(typeof(IDataRetriever<GeoLocation>), typeof(GeoLocationDataRetriever));
-            services.TryAddScoped(typeof(IOnlineDataRetriever), typeof(GeoLocationDataRetriever));
-            services.TryAddScoped(typeof(IOfflineDataRetriever<GeoLocation>), typeof(GeoLocationDataRetriever));
             return services;
         }
     }
